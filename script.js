57d6663a8058d115a9c22fed549df879afbcd015
Ganesh Pawar <ganeshpawar5070@gmail.com>
2026-07-08 18:01:47 +0530

    Add login and upload/download functions in script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const SERVER = "https://pine-forty-wide-caroline.trycloudflare.com";
+
+function login() {
+
+    const u = document.getElementById("username").value;
+    const p = document.getElementById("password").value;
+
+    if (u === "sumo" && p === "Sumo@123") {
+
+        document.getElementById("loginBox").style.display = "none";
+        document.getElementById("homeBox").style.display = "block";
+
+    } else {
+
+        document.getElementById("msg").innerText = "Wrong Username or Password";
+
+    }
+
+}
+
+function openUpload() {
+    window.open(SERVER + "/upload", "_blank");
+}
+
+function openDownload() {
+    window.open(SERVER + "/download", "_blank");
+}
